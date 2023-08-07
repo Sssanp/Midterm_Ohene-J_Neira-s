2bde9180ba112726485c83b5da9b959d9fe4ece8
j-ohene <jesicaohene1@gmail.com>
2023-08-07 11:31:08 -0400

    about page icons separate device classes for recipes for alignment

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-window.onload = function(){
-    window.addEventListener('scroll', function (e) {
-        if (window.scrollY> 100) {
-            document.querySelector("header").classList.add('is-scrolling');
-        } else{
-            document.querySelector("header").classList.remove('is-scrolling');  
-        }
-    });
+function increaseItem() {
+    itemCount++;
+    updateItemCount();
+  }
+  
+  function decreaseItem() {
+    if (itemCount > 0) {
+      itemCount--;
+      updateItemCount();
+    }
+  }
+  
+  function updateItemCount() {
+    document.getElementsByClassName('item-count').textContent = itemCount;
+  }
+  
+  function addToCart() {
+    alert("Added " + itemCount + " item(s) to cart!");
+  }
 
 
 
-const hamburgerIMG = document.querySelector(".hamburgerIMG");
-const showMenu = document.querySelector (".show-menu");
 
-hamburgerIMG.addEventListener('click', function (){
-    hamburgerIMG.classList.toggle('show-menu');
-    showMenu.classList.toggle('show-menu');
-});
-}
+
+
+
+    
